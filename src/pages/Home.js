2b5a5a7f81141abac67f5aee99d2283ca6024ce4
megaseves <megaseves@gmail.com>
2023-05-29 01:38:36 +0200

    Add: tablet view on footer, nav, contact, home pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -20,27 +20,28 @@ function Home() {
 
                 </div>
                 <div className="hero">
-                    <div className="myFace">
-
-                    </div>
-                    {/*<img src="myFace.jpg" alt="hero" className={"hero-img"}/>*/}
+                    <div className="myFace"></div>
                 </div>
             </div>
             <div className="tech-stack">
                 <p>Tech Stack</p>
                 <div className="stacks">
-                    <img src="../icons/html.png" alt="html" title="HTML5" className={"stack"}/>
-                    <img src="../icons/css.png" alt="css" title="CSS3" className={"stack"}/>
-                    <img src="../icons/js.png" alt="js" title="Javascript" className={"stack"}/>
-                    <img src="../icons/react.png" alt="react" title="ReactJS" className={"stack"}/>
-                    <img src="../icons/vue.png" alt="vue" title="VueJS" className={"stack"}/>
-                    <img src="../icons/node.png" alt="node" title="NodeJS" className={"stack"}/>
-                    <img src="../icons/python.png" alt="python" title="Python" className={"stack"}/>
-                    <img src="../icons/java.png" alt="java" title="Java" className={"stack"}/>
-                    <img src="../icons/postgresql.png" alt="postgresql" title="PostgreSQL" className={"stack"}/>
-                    <img src="../icons/git.png" alt="git" title="Git" className={"stack"}/>
-                    <img src="../icons/heroku.png" alt="heroku" title="Heroku" className={"stack"}/>
-                    <img src="../icons/firebase.png" alt="firebase" title="Firebase" className={"stack"}/>
+                    <span className="first-stacks">
+                        <img src="../icons/html.png" alt="html" title="HTML5" className={"stack"}/>
+                        <img src="../icons/css.png" alt="css" title="CSS3" className={"stack"}/>
+                        <img src="../icons/js.png" alt="js" title="Javascript" className={"stack"}/>
+                        <img src="../icons/react.png" alt="react" title="ReactJS" className={"stack"}/>
+                        <img src="../icons/vue.png" alt="vue" title="VueJS" className={"stack"}/>
+                        <img src="../icons/node.png" alt="node" title="NodeJS" className={"stack"}/>
+                    </span>
+                    <span className="second-stacks">
+                        <img src="../icons/python.png" alt="python" title="Python" className={"stack"}/>
+                        <img src="../icons/java.png" alt="java" title="Java" className={"stack"}/>
+                        <img src="../icons/postgresql.png" alt="postgresql" title="PostgreSQL" className={"stack"}/>
+                        <img src="../icons/git.png" alt="git" title="Git" className={"stack"}/>
+                        <img src="../icons/heroku.png" alt="heroku" title="Heroku" className={"stack"}/>
+                        <img src="../icons/firebase.png" alt="firebase" title="Firebase" className={"stack"}/>
+                    </span>
                 </div>
             </div>
 
