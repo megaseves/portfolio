fa1c89d9d3bb8c1f2bee08249d0ddb6aa4d16bf7
megaseves <megaseves@gmail.com>
2023-05-28 15:19:47 +0200

    Add: Projects page with project card design

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 import About from "./pages/About";
+import Projects from "./pages/Projects";
 
-library.add(faLinkedin, faGithub);
+library.add(faLinkedin, faGithub, faArrowUpRightFromSquare);
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
         <Navbar />
         <Home />
         <About />
+        <Projects />
     </div>
   );
 }
---
+++ src/pages/Projects.js
@@ -0,0 +1,40 @@
+import '../styles/Projects.css';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+function Projects() {
+    return (
+        <div className="Projects" id={"projects"}>
+           <div className="projects-content">
+
+                <div className="projects-header">
+                    <h4>PORTFOLIO</h4>
+                </div>
+
+               <div className="projects">
+
+                    <div className="project-card">
+                        <div className="img-card">
+                            <img src="../developing.jpg" alt="developing"/>
+                        </div>
+                        <div className="text-card">
+                            <h3>Fast Food 🍔</h3>
+                            <p>This is a restaurant website. It is fully responsive. We can use it on any device. I used viewport width and height to create it all.</p>
+                            <div className="card-stacks">
+                                <span className="project-stack">React</span>
+                                <span className="project-stack">Vanila CSS</span>
+                            </div>
+                            <div className="card-links">
+                                <a href={"https://github.com/megaseves/restaurant-react"} target={"_blank"} rel="noreferrer" className="project-code">Code <FontAwesomeIcon icon="fa-brands fa-github" /></a>
+                                <a href={"https://github.com/megaseves/restaurant-react"} target={"_blank"} rel="noreferrer" className="project-code">Live Demo <FontAwesomeIcon icon="fa-solid fa-arrow-up-right-from-square" /></a>
+                            </div>
+                        </div>
+                    </div>
+
+               </div>
+
+           </div>
+        </div>
+    );
+}
+
+export default Projects;
