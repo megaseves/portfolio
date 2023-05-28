579c89acb00ac44001d6b50588f9a5d2fdcccc03
megaseves <megaseves@gmail.com>
2023-05-28 22:58:25 +0200

    Add: Footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,16 @@
 import '../styles/Footer.css';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 function Footer() {
     return (
         <div className="Footer">
-
+            <div className="footer-content">
+                <div className="copyright">Copyright © 2023. All rights are reserved</div>
+                <span className="links">
+                    <a href="https://www.linkedin.com/in/lufy/" target={"_blank"} rel="noreferrer" ><FontAwesomeIcon icon="fa-brands fa-linkedin" /></a>
+                    <a href="https://github.com/megaseves/" target={"_blank"} rel="noreferrer" ><FontAwesomeIcon icon="fa-brands fa-github" /></a>
+                </span>
+            </div>
         </div>
     );
 }
