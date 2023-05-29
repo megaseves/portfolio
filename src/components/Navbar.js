a1cdab5213d2631b50e246656baa0e1adbe6243a
megaseves <megaseves@gmail.com>
2023-05-29 02:25:44 +0200

    Add: tablet view navbar setup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import '../styles/Navbar.css';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 function Navbar() {
     return (
@@ -11,6 +12,17 @@ function Navbar() {
                 <li><a href="#projects">Projects</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
+            <FontAwesomeIcon className={"navbar-menu-btn"} icon="fa-solid fa-bars" />
+
+            <div className="mobile-nav closed-menu">
+                <div className="mobile-nav-header"><FontAwesomeIcon className={"navbar-menu-btn"} icon="fa-solid fa-x" /></div>
+                <div className="mobile-nav-menu">
+                    <a href="#home">Home</a>
+                    <a href="#about">About</a>
+                    <a href="#projects">Projects</a>
+                    <a href="#contact">Contact</a>
+                </div>
+            </div>
         </div>
     );
 }
