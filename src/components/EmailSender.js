ffcacb5eade072193e4979651f7019649f609396
megaseves <megaseves@gmail.com>
2023-08-17 17:47:34 +0200

    Add: email sender error handling and sent email feedback

--- src/components/EmailSender.js
+++ src/components/EmailSender.js
@@ -30,13 +30,47 @@ function EmailSender() {
         e.preventDefault();
         if (name.trim().length === 0 || email.trim().length === 0 || emailMessage.trim().length === 0) {
             console.log("Input value is empty!");
+            openErrorMessage();
         } else {
             sendEmail(e);
+            togglePopup();
+            closeErrorMessage();
         }
-    }
+    };
+
+    const togglePopup = () => {
+        const popup = document.querySelector('.popup');
+        if (popup.classList.contains("close")) {
+            popup.classList.remove("close");
+        } else {
+            popup.classList.add("close");
+        }
+    };
+
+    const openErrorMessage = () => {
+        const errorMessage = document.querySelector('.error-message-form');
+        if (errorMessage.classList.contains("close")) {
+            errorMessage.classList.remove("close");
+        }
+    };
+
+    const closeErrorMessage = () => {
+        const errorMessage = document.querySelector('.error-message-form');
+        if (!errorMessage.classList.contains("close")) {
+            errorMessage.classList.add("close");
+        }
+    };
 
     return (
         <div className='email-form-container'>
+            <div className='popup close' onClick={togglePopup}>
+                <img src='letter.png' alt='email letter' width={'220px'}></img>
+                <h3>THANKS FOR YOUR EMAIL</h3>
+                <p>You have successfully sent a message, I will get in touch soon as possible.</p>
+            </div>
+
+            <div className='error-message-form close' onClick={closeErrorMessage}>All fields must be filled in!</div>
+
             <form ref={form} onSubmit={sendIfIsNotEmptyField} className='email-form'>
                 <label>Name</label>
                 <input className='form-input' type="text" name="user_name" onChange={(event)=> setName(event.target.value)}/>
